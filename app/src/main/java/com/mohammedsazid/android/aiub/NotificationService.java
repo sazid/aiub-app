9c7396319ee7955d905ef47fbc1c7a8aab757d69
sazid <sazidozon@gmail.com>
2018-09-18 23:29:43 +0600

    Add mechanism for stopping the service after enough tries

--- app/src/main/java/com/mohammedsazid/android/aiub/NotificationService.java
+++ app/src/main/java/com/mohammedsazid/android/aiub/NotificationService.java
@@ -3,7 +3,6 @@ package com.mohammedsazid.android.aiub;
 import android.annotation.TargetApi;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,13 +10,11 @@ import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.Handler;
-import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.JobIntentService;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.ViewGroup;
@@ -25,6 +22,10 @@ import android.view.WindowManager;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.crashlytics.android.Crashlytics;
 
 
 public class NotificationService extends JobIntentService {
@@ -33,9 +34,10 @@ public class NotificationService extends JobIntentService {
     private CustomWebView webView;
     private SharedPreferences prefs;
 
+    private boolean SHOULD_STOP = false;
+
     public static void enqueue(Context context) {
         Intent intent = new Intent(context, NotificationService.class);
-//        ContextCompat.startForegroundService(context, intent);
         enqueueWork(context, NotificationService.class, 23, intent);
     }
 
@@ -53,7 +55,7 @@ public class NotificationService extends JobIntentService {
                 .setWhen(System.currentTimeMillis())
                 .setContentTitle("Checking for new notifications");
 
-        Log.d("NotificationServce", "Starting notification service...");
+        Log.d("NotificationService", "Starting notification service...");
         startForeground(2, builder.build());
 
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
@@ -92,11 +94,39 @@ public class NotificationService extends JobIntentService {
             @Override
             public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
                 if (message.contentEquals(WRONG_DETAILS_MSG)) {
-                    stopSelf();
+                    SHOULD_STOP = true;
                     return true;
                 }
 
                 return super.onJsAlert(view, url, message, result);
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                switch (webView.getUrl()) {
+                    case "https://portal.aiub.edu/Student":
+                    case "https://portal.aiub.edu/Student/":
+                        Log.d("NotificationService", "Logged in");
+                        toast("Logged in");
+                        webView.loadUrl("https://portal.aiub.edu/Student/Notification");
+                        break;
+                    case "https://portal.aiub.edu/Student/Notification":
+                    case "https://portal.aiub.edu/Student/Notification/":
+                        toast("Loaded notifications");
+                        SHOULD_STOP = true;
+                        break;
+                    case "https://portal.aiub.edu/":
+                        Log.d("NotificationService", "Logging in...");
+                        login(view, url);
+                        break;
+                    default:
+                        // not supported
+                        Crashlytics.log(1, "Unsupported URL", webView.getUrl());
+                        break;
+                }
             }
         });
 
@@ -106,10 +136,43 @@ public class NotificationService extends JobIntentService {
             }
         } catch (Exception e) {
             e.printStackTrace();
+            SHOULD_STOP = true;
+            return;
         }
 
-        stopSelf();
+        webView.loadUrl("https://portal.aiub.edu/");
     }
+
+    private void toast(String msg) {
+        Toast.makeText(NotificationService.this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void login(WebView view, String url) {
+        String username = getUsername();
+        String password = getPassword();
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password))
+            return;
+
+        if (url.contentEquals("https://portal.aiub.edu/") || url.startsWith("https://portal.aiub.edu/Login")) {
+            String FIELD_USERNAME_ID = "username";
+            String FIELD_PASSWORD_ID = "password";
+
+            String jsScript =
+                    "if ($('.text-danger').length !== 0) {" +
+                            "window.alert('" + WRONG_DETAILS_MSG + "');" +
+                            "} else {" +
+                            "$('#" + FIELD_USERNAME_ID + "').val('" + username + "');" +
+                            "$('#" + FIELD_PASSWORD_ID + "').val('" + password + "');" +
+                            "$('button').first().click();" +
+                            "}";
+
+
+            new Handler(getMainLooper()).postDelayed(() ->
+                    view.loadUrl("javascript: {" + jsScript + "};"), 500);
+        }
+    }
+
 
     @NonNull
     @TargetApi(26)
@@ -127,7 +190,7 @@ public class NotificationService extends JobIntentService {
         if (mNotificationManager != null) {
             mNotificationManager.createNotificationChannel(mChannel);
         } else {
-            stopSelf();
+            SHOULD_STOP = true;
         }
         return channelId;
     }
@@ -155,5 +218,13 @@ public class NotificationService extends JobIntentService {
     protected void onHandleWork(@NonNull Intent intent) {
         new Handler(getMainLooper())
                 .post(() -> createAndAttachView(this));
+
+        int tries = 0;
+        try {
+            while (!SHOULD_STOP && tries++ < 90) {
+                Thread.sleep(1000);
+            }
+        } catch (Exception ignored) {
+        }
     }
 }
